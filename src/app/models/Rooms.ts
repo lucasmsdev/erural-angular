399d17e7bd4528ba32e2b83ca9e24212945bbbf3
LucasMachado9 <lucasdevoficial@gmail.com>
2022-11-20 23:15:36 -0300

    final commit

---
+++ src/app/models/Rooms.ts
@@ -0,0 +1,13 @@
+import { Comment } from "./Comment";
+import { User } from "./user";
+export interface Room{
+  id: number;
+  name: string;
+  description: string;
+  image_url: string;
+  youtube_url: string;
+  comments: Comment[];
+  user: User;
+
+
+}
